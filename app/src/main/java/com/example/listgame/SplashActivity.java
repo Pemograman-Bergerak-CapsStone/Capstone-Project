a49453480565fa7d7da58784a266c4c04338c318
mrifkiali <rifki.ali31@gmail.com>
2020-11-20 19:23:44 +0700

    Revisi

--- app/src/main/java/com/example/listgame/SplashActivity.java
+++ app/src/main/java/com/example/listgame/SplashActivity.java
@@ -2,27 +2,33 @@ package com.example.listgame;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 finish();
             }
-        }, 2000);
-
+        }, 2000L); //3000 L = 3 detik
     }
 }
